afca9b88c574d565094138ca05bff46cdf61f936
sheinDihayco <dihayco020903@gmail.com>
2024-10-20 00:14:10 +0800

    ok

--- MobileAppDevelopment/src/views/Floating/index.vue
+++ MobileAppDevelopment/src/views/Floating/index.vue
@@ -5,7 +5,7 @@
         <ion-buttons slot="start">
           <ion-back-button></ion-back-button>
         </ion-buttons>
-        <ion-title>Card Page</ion-title>
+        <ion-title> Floating Action Button </ion-title>
       </ion-toolbar>
     </ion-header>
 
--- MobileAppDevelopment/src/views/Modal/index.vue
+++ MobileAppDevelopment/src/views/Modal/index.vue
@@ -5,7 +5,7 @@
         <ion-buttons slot="start">
           <ion-back-button></ion-back-button>
         </ion-buttons>
-        <ion-title>Card Page</ion-title>
+        <ion-title>Modal</ion-title>
       </ion-toolbar>
     </ion-header>
 
