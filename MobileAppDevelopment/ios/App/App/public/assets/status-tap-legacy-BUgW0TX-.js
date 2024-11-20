c63fc5a8084a516d3a489dc6b49778e6686f6053
sheinDihayco <dihayco020903@gmail.com>
2024-11-21 01:03:01 +0800

    Generating APK

---
+++ MobileAppDevelopment/ios/App/App/public/assets/status-tap-legacy-BUgW0TX-.js
@@ -0,0 +1,5 @@
+System.register(["./index-legacy-D7ENu4km.js"],(function(e,t){"use strict";var n,r,s,i,o;return{setters:[e=>{n=e.af,r=e.ag,s=e.ah,i=e.ai,o=e.aj}],execute:function(){
+/*!
+             * (C) Ionic http://ionicframework.com - MIT License
+             */
+e("startStatusTap",(()=>{const e=window;e.addEventListener("statusTap",(()=>{n((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=r(a);c&&new Promise((e=>s(c,e))).then((()=>{i((async()=>{c.style.setProperty("--overflow","hidden"),await o(c,300),c.style.removeProperty("--overflow")}))}))}))}))}))}}}));
